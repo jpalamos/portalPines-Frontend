9f4080a5c99df265c23f20e64a67a689fa2fcbbd
Jorge Palacios <jpm.electronica@gmail.com>
2020-07-31 16:00:09 -0500

    v1.2

---
+++ precache-manifest.70ddd244296f03f0b6836d96c2fb8f23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abca964f030fbd881c98648d191bf3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4e8a2ffcb55bf70c894",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "037fc0157ad2b3858af5",
+    "url": "/static/css/main.31ac9b7b.chunk.css"
+  },
+  {
+    "revision": "b4e8a2ffcb55bf70c894",
+    "url": "/static/js/2.6a05ad0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6a05ad0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037fc0157ad2b3858af5",
+    "url": "/static/js/main.1619dbe3.chunk.js"
+  },
+  {
+    "revision": "13b122f953aa832e0899",
+    "url": "/static/js/runtime-main.b4212f8b.js"
+  }
+]);
